010790593ab4b7e5fe149b0b8b549a6af1217ef5
BlackSeagull3104 <qma662@connect.hkust-gz.edu.cn>
2026-09-12 05:29:52 +0000

    Complete Task 2 character-level training

--- config/train_shakespeare_char_small.py
+++ config/train_shakespeare_char_small.py
@@ -2,8 +2,8 @@
 # good for debugging and playing on macbooks and such
 
 out_dir = 'out-shakespeare-char'
-eval_interval = 250 # keep frequent because we'll overfit
-eval_iters = 200
+eval_interval = 100 # keep frequent because we'll overfit
+eval_iters = 50
 log_interval = 10 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
@@ -15,8 +15,8 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'shakespeare_char'
 gradient_accumulation_steps = 1
-batch_size = 64
-block_size = 256 # context of up to 256 previous characters
+batch_size = 32
+block_size = 128 # context of up to 256 previous characters
 
 # baby GPT model :)
 n_layer = 4
@@ -26,11 +26,11 @@ dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
 max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+lr_decay_iters = 1000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
-warmup_iters = 100 # not super necessary potentially
+warmup_iters = 50 # not super necessary potentially
 
 # on macbook also add
 # device = 'cpu'  # run on cpu only
